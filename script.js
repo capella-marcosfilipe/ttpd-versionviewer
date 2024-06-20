d161fdcf32b18e4920678dcada6fc5d8ebfd3ae6
Marcos Filipe Capella <marcoscapella@outlook.com>
2024-06-20 15:59:42 -0300

    Final Commit - 1st version

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 import ttpdData from "./data/ttpdData.js";
+
+const html = document.querySelector("html");
+const manuscriptBt = document.querySelector(".manuscript-button");
+const bolterBt = document.querySelector(".bolter-button");
+const albatrossBt = document.querySelector(".albatross-button");
+const blackDogBt = document.querySelector(".blackdog-button");
+const anthologyBt = document.querySelector(".anthology-button");
+const buttons = document.querySelectorAll(".column-selector__button");
 const tracksViewer = document.querySelector(
   ".column-viewer__description__album-tracks"
 );
@@ -19,7 +27,7 @@ const albumCoverPath = ttpdData[0].albumCoverPath.toString();
 versionCover.setAttribute("src", albumCoverPath);
 
 // Adding the release date to the page
-const releaseDate = new Date(ttpdData[0].releaseDate);
+let releaseDate = new Date(ttpdData[0].releaseDate);
 const releaseDateOptions = { year: "numeric", month: "long", day: "numeric" };
 versionReleaseDate.innerHTML += releaseDate.toLocaleDateString(
   "en-US",
@@ -27,12 +35,79 @@ versionReleaseDate.innerHTML += releaseDate.toLocaleDateString(
 );
 
 // Adding tracks and bonus tracks to the page
-const trackItemsHTML = ttpdData[0].tracks
+let trackItemsHTML = ttpdData[0].tracks
   .map((track) => `<li>${track}</li>`)
   .join("");
 
-const bonusTracksItemsHTML = ttpdData[0].bonusTracks
+let bonusTracksItemsHTML = ttpdData[0].bonusTracks
   .map((bonus) => `<li class="track-bonus">${bonus}</li>`)
   .join("");
 
 tracksViewer.innerHTML = trackItemsHTML + bonusTracksItemsHTML;
+
+// Actions for the buttons
+
+function changeVersion(version) {
+  buttons.forEach((button) => button.classList.remove("active"));
+  html.setAttribute("version-contexto", version);
+  versionCover.setAttribute("src", `../assets/${version}.png`);
+  switch (version) {
+    case "the-manuscript":
+      setVersionAttributes(0);
+      break;
+    case "the-bolter":
+      setVersionAttributes(1);
+      break;
+    case "the-albatross":
+      setVersionAttributes(2);
+      break;
+    case "the-black-dog":
+      setVersionAttributes(3);
+      break;
+    case "the-anthology":
+      setVersionAttributes(4);
+      break;
+    default:
+      break;
+  }
+}
+
+function setVersionAttributes(index) {
+  versionTitle.innerHTML = "filename: " + ttpdData[index].versionTitle;
+  releaseDate = new Date(ttpdData[index].releaseDate);
+  versionReleaseDate.innerHTML =
+    "Release Date: " +
+    releaseDate.toLocaleDateString("en-US", releaseDateOptions);
+  trackItemsHTML = ttpdData[1].tracks
+    .map((track) => `<li>${track}</li>`)
+    .join("");
+  bonusTracksItemsHTML = ttpdData[index].bonusTracks
+    .map((bonus) => `<li class="track-bonus">${bonus}</li>`)
+    .join("");
+  tracksViewer.innerHTML = trackItemsHTML + bonusTracksItemsHTML;
+}
+
+manuscriptBt.addEventListener("click", () => {
+  changeVersion("the-manuscript");
+  manuscriptBt.classList.add("active");
+});
+
+bolterBt.addEventListener("click", () => {
+  changeVersion("the-bolter");
+  bolterBt.classList.add("active");
+});
+
+albatrossBt.addEventListener("click", () => {
+  changeVersion("the-albatross");
+  albatrossBt.classList.add("active");
+});
+
+blackDogBt.addEventListener("click", () => {
+  changeVersion("the-black-dog");
+  blackDogBt.classList.add("active");
+});
+
+anthologyBt.addEventListener("click", () => {
+  changeVersion("the-anthology");
+  anthologyBt.classList.add("active");
+});
